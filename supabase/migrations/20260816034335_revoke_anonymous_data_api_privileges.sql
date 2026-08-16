6c7b5626253ed0423880ca5a58fcb9f618c70230
Jazzygmack <dynamackops@gmail.com>
2026-08-16 18:22:51 -0400

    Sync revoke_anonymous_data_api_privileges migration

---
+++ supabase/migrations/20260816034335_revoke_anonymous_data_api_privileges.sql
@@ -0,0 +1,3 @@
+revoke all privileges on all tables in schema public from anon;
+revoke all privileges on all sequences in schema public from anon;
+revoke execute on all functions in schema public from anon;
